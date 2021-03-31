d39275daaa01d7bfc15beca584a591613bcbf077
JoannaPia <paczesna.joanna@gmail.com>
2021-03-31 19:05:37 +0200

    new

---
+++ Warcaby/Warcaby/Board.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warcaby
+{
+    class Board
+    {
+        private Pawn[,] _board;
+        private int size;
+        public Pawn[,] board
+        {
+            get { return _board; }
+        }
+        public int Size
+        {
+            get { return size; }
+        }
+        public Board()
+        {
+            Console.WriteLine("Please input the size of the board (10-20): ");
+            size = Convert.ToInt32(Console.ReadLine());
+            _board = new Pawn[size, size];
+            InitBoard();
+            Console.WriteLine(this.ToString());
+        }
+
+        public void InitBoard()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    List<int> allowedRows = new List<int>() { 0, 1, 2, 3, size - 1, size - 2, size - 3, size - 4 };
+                    if (allowedRows.Contains(i))
+                    {
+                        /* sprawdzanie czy wiersz (czyli rzad planszy) jest parzysty*/
+                        if (i % 2 == 0)
+                        {
+                            if (j % 2 == 0)
+                            {
+                                if (i > size / 2)
+                                    board[i, j] = new Pawn(true, i, j);
+                                else
+                                    board[i, j] = new Pawn(false, i, j);
+                            }
+                            else
+                                board[i, j] = null;
+                        }
+                        else
+                        {
+                            /* sprawdzanie czy jest nieparzysty*/
+                            if (j % 2 == 1)
+                            {
+                                if (i > size / 2)
+                                    board[i, j] = new Pawn(true, i, j);
+                                else
+                                    board[i, j] = new Pawn(false, i, j);
+                            }
+                            else
+                                board[i, j] = null;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string b = "";
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (board[i, j] == null)
+                        //Console.Write("O ");
+                        b += "O ";
+                    else
+                    {
+                        string pionek = board[i, j].IsWhite ? "W " : "B ";
+                        //Console.Write(pionek + " ");
+                        b += pionek;
+                    }
+                }
+                b += "\n";
+            }
+
+            return b;
+        }
+
+        public void RemovePawn()
+        {
+
+        }
+
+        public void MovePawn()
+        {
+
+        }
+
+    }
+
+}
---
+++ Warcaby/Warcaby/Game.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warcaby
+{
+
+    /*
+There is a class Game which contains all game logic and actions.
+There is a method Start() that starts whole game between players.
+There is a method Round() that determines one round actions i.e. check who plays next or is there a winner yet.
+There is a method that checks if starting position from user input is a valid pawn and if ending position is within board boundaries. If so, it calls TryToMakeMove() on pawn instance.
+There is a method CheckForWinner() that checks after each round is there an a winner.
+Method CheckForWinner() checks also for draws.
+    */
+    class Game
+    {
+        private Board board;
+        private bool isFinished = false;
+
+     
+        public void Start()
+        {
+            board = new Board();
+            while (!isFinished) 
+            { 
+                Round(); 
+            }
+        }
+
+        public void Round()
+        {
+            TryToMakeMove();
+            CheckForWinner();
+        }
+
+        public void TryToMakeMove()
+        {
+
+        }
+
+        public void CheckForWinner()
+        {
+            int W = 0, B = 0;
+
+            for (int row = 0; row < board.Size; row++)
+            {
+                for (int col = 0; col < board.Size; col++)
+                {
+                    Pawn p = board.board[row, col];
+                    if (board.board[row,col] != null)
+                    {
+                        if (board.board[row, col].IsWhite)
+                            W++;
+                        else
+                            B++;
+                    }
+                }
+            }
+
+            ValidateResult(W, B);
+        }
+
+        public void ValidateResult(int white, int black)
+        {
+            if (white == 1 && black == 1)
+            {
+                printEndScreen(3);
+                isFinished = true;
+            }
+            else if (white == 0)
+            {
+                printEndScreen(2);
+                isFinished = true;
+            }
+            else if (black == 0)
+            {
+                printEndScreen(1);
+                isFinished = true;
+            }
+
+            isFinished = false;
+        }
+
+        private void printEndScreen(int ending)
+        {
+            if (ending == 1)
+            {
+                Console.WriteLine("**************White has won**************");
+            }
+            else if (ending == 2)
+            {
+                Console.WriteLine("**************Black has won**************");
+            }
+            else
+            {
+                Console.WriteLine("************** Tied **************"); 
+            }
+        }
+    }
+}
