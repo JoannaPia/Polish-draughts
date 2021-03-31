a59adc82e2ed028b5771a4b16138ee8de315d694
JoannaPia <paczesna.joanna@gmail.com>
2021-03-31 18:39:03 +0200

    new

---
+++ Warcaby/Warcaby/Pawn.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace Warcaby
+{
+    class Pawn
+    {
+        private bool isWhite;
+        public bool IsWhite
+        {
+            get { return isWhite; }
+            set { isWhite = value; }
+        }
+
+        private bool isCrowned;
+        public bool IsCrowned
+        {
+            get { return isCrowned; }
+            set { isCrowned = value; }
+        }
+
+        Tuple<int, int> coordinates;
+
+        public Pawn(bool _isWhite, int x, int y)
+        {
+            coordinates = Tuple.Create(x, y);
+            isWhite = _isWhite;
+        }
+
+        public bool ValidateMove()
+        {
+            // check if multiple jumps are available
+            return true;
+        }
+    }
+
+}
